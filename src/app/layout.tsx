2e11196b6f763667003510a6769300c08c0a9383
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-02-24 23:19:41 +0100

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
