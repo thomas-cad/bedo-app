44c3484345e44fd698975a6d34fc0b06c6fe20e1
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-02-24 22:49:21 +0100

    Add BannerShop component and enhance Navbar styles for active links

---
+++ src/app/components/BannerShop.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BannerShop = () => {
+    return (
+        <div className="relative w-full h-[75vh] overflow-hidden">
+            <img
+                src="/image/shop/banner.jpg"
+                alt="Banner"
+                className="w-full h-full object-cover object-top" // object-top pour aligner l'image en haut
+            />
+        </div>
+    );
+};
+
+export default BannerShop;
+
