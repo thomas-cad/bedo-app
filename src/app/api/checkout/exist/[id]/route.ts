5438fb887f7118ccbd5afc7258f9f5fd75dd947e
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-03-07 14:19:30 +0100

    Add error handling for missing order ID in checkout API

--- src/app/api/checkout/exist/[id]/route.ts
+++ src/app/api/checkout/exist/[id]/route.ts
@@ -4,7 +4,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    // Ensure id is provided in the params
     const orderId = params.id;
+
+    if (!orderId) {
+        return NextResponse.json({ error: 'Order ID is required' }, { status: 400 });
+    }
 
     try {
         const order = await prisma.order.findUnique({
@@ -15,9 +20,9 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
             return NextResponse.json({ error: 'Order not found' }, { status: 404 });
         }
 
-        return NextResponse.json({ success: true });
+        return NextResponse.json({ success: true, order });
     } catch (error) {
-        console.error('Error fetching item:', error); // Log the error for debugging
+        console.error('Error fetching order:', error); // Log the error for debugging
         return NextResponse.json({ error: 'Internal Server Error', details: (error as any).message }, { status: 500 });
     }
 }
