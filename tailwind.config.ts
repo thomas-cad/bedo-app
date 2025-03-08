4b537dd11216670bca32330e31b6b7d6605c7e53
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-03-08 20:55:31 +0100

    Refactor docker-compose command for clarity and update Tailwind CSS configuration to include purge settings and third-party styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,15 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Add paths to third-party library CSS files here (optional)
+    "./node_modules/@brainhubeu/react-carousel/lib/style.css",
+    "./node_modules/react-toastify/dist/*.css",
   ],
   theme: {
     extend: {
       maskImage: {
-        "gradient": "linear-gradient(to bottom, black 70%, transparent 100%)",
+        gradient: "linear-gradient(to bottom, black 70%, transparent 100%)",
       },
       colors: {
         background: "var(--background)",
@@ -18,4 +22,25 @@ export default {
     },
   },
   plugins: [],
+
+  // Add purge configuration to control CSS purging
+  purge: {
+    enabled: process.env.NODE_ENV === "production", // Enable purging only in production
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+      // Add paths to third-party library CSS files here (optional)
+      "./node_modules/@brainhubeu/react-carousel/lib/style.css",
+      "./node_modules/react-toastify/dist/*.css",
+    ],
+    options: {
+      safelist: [
+        // Whitelist specific classes if needed
+        "whitelist-class",
+        "another-whitelisted-class",
+      ],
+    },
+  },
 } satisfies Config;
