319bf9a1cd0d699a435cba1f3eb64fc34a129936
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-03-08 21:24:38 +0100

    Refactor Navbar component and update Tailwind CSS configuration for improved structure and clarity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,6 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Add paths to third-party library CSS files here (optional)
     "./node_modules/@brainhubeu/react-carousel/lib/style.css",
     "./node_modules/react-toastify/dist/*.css",
   ],
@@ -22,25 +20,4 @@ export default {
     },
   },
   plugins: [],
-
-  // Add purge configuration to control CSS purging
-  purge: {
-    enabled: process.env.NODE_ENV === "production", // Enable purging only in production
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-      // Add paths to third-party library CSS files here (optional)
-      "./node_modules/@brainhubeu/react-carousel/lib/style.css",
-      "./node_modules/react-toastify/dist/*.css",
-    ],
-    options: {
-      safelist: [
-        // Whitelist specific classes if needed
-        "whitelist-class",
-        "another-whitelisted-class",
-      ],
-    },
-  },
 } satisfies Config;
