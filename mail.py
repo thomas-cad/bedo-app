b02e46152ab73b12789b2b8b73b2d00fe9498bb8
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-03-10 00:30:45 +0100

    Simplify SMTP email sending logic and remove unnecessary debug output

--- mail.py
+++ mail.py
@@ -9,37 +9,15 @@ load_dotenv()
 SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = int(os.getenv("SMTP_PORT"))
 SMTP_USER = os.getenv("SMTP_USER")
-SMTP_PASS = os.getenv("SMTP_PASS")
+SMTP_PASS = os.getenv("SMTP_PASS")                        
 
-try:
-    # Connexion au serveur SMTP
-    print(f"SMTP_HOST: {SMTP_HOST}")
-    print(f"SMTP_PORT: {SMTP_PORT}")
-    print(f"SMTP_USER: {SMTP_USER}")
-    print(f"SMTP_PASS: {SMTP_PASS}")
-    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
-        print(f"Connexion réussie au serveur SMTP {SMTP_HOST}:{SMTP_PORT}")
-        # Afficher les variables d'environnement
-        
-        # Démarrer la connexion TLS si nécessaire
-        server.starttls()
-        print("Connexion TLS démarrée.")
-
-        # Authentification
-        server.login(SMTP_USER, SMTP_PASS)
-        print("Authentification réussie.")
-
-        # Test d'envoi d'un email
-        sender_email = SMTP_USER
-        receiver_email = "thomas.cadegros@telecom-paris.fr"  # Remplacez par l'adresse du destinataire
-        subject = "Test SMTP"
-        body = "Ceci est un test d'envoi d'email via SMTP."
-
-        message = f"Subject: {subject}\n\n{body}"
-        server.sendmail(sender_email, receiver_email, message)
-        print("Email envoyé avec succès.")
-
-except smtplib.SMTPException as e:
-    print(f"Erreur SMTP: {e}")
-except Exception as e:
-    print(f"Erreur inattendue: {e}")
+smtpServer=SMTP_HOST      
+fromAddr=SMTP_USER         
+port = SMTP_PORT
+toAddr='thomas.cadegros@telecom-paris.fr'     
+text= "This is a test of sending email from within Python."
+server = smtplib.SMTP(smtpServer,port)
+server.ehlo()
+server.starttls()
+server.sendmail(fromAddr, toAddr, text) 
+server.quit()
