55d7518b7f11a31e3af7a4471876c3d7d00ef3f6
Thomas Cadegros <thomas.cadegros@telecom-paris.fr>
2025-03-10 14:09:37 +0100

    Remove unused imports and commented-out code in HomePage component for improved clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useEffect } from "react";
+
 import dynamic from "next/dynamic";
 
 // Importation des composants avec lazy loading
 const HeroSection = dynamic(() => import("@/app/components/HeroSection"), { ssr: false });
 // const SubscribeSection = dynamic(() => import("@/app/components/SubscribeSection"), { ssr: false });
-const VideoSection = dynamic(() => import("@/app/components/VideoSection"), { ssr: false });
+// const VideoSection = dynamic(() => import("@/app/components/VideoSection"), { ssr: false });
 // const TeamSection = dynamic(() => import("@/app/components/TeamSection"), { ssr: false });
 // const TextSection = dynamic(() => import("@/app/components/TextSection"), { ssr: false });
 
 export default function HomePage() {
   // const [showSubscribe, setShowSubscribe] = useState(false);
-  const [showVideo, setShowVideo] = useState(false);
+  // const [showVideo, setShowVideo] = useState(false);
   // const [showTeam, setShowTeam] = useState(false);
 
   // Chargement progressif des composants
   useEffect(() => {
     // setTimeout(() => setShowSubscribe(true), 50);
-    setTimeout(() => setShowVideo(true), 100);
+    // setTimeout(() => setShowVideo(true), 100);
     // setTimeout(() => setShowTeam(true), 200);
   }, []);
 
@@ -28,7 +30,7 @@ export default function HomePage() {
       <HeroSection />
 
       {/* {showSubscribe && <SubscribeSection />} */}
-      {showVideo && <VideoSection />}
+      {/* {showVideo && <VideoSection />} */}
       {/* {showTeam && <TeamSection />}   */}
       {/* {showTeam && <TextSection />}  */}
     </div>
